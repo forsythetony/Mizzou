83a32eee84c2b847a93c0400d010179563d338d2
forsythetony <forsythetony@gmail.com>
2014-01-30 18:29:54 -0600

    Laid out all of the view controllers

---
+++ Mizzou/PanelViewController.h
@@ -0,0 +1,13 @@
+//
+//  PanelViewController.h
+//  Mizzou
+//
+//  Created by Anthony Forsythe on 1/30/14.
+//  Copyright (c) 2014 ARF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanelViewController : UITableViewController
+
+@end
